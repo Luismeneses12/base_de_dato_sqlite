95e25297f08a19ae0d49f9d69c37b1027134643c
lmeneses <luis.0495@hotmail.com>
2025-03-26 10:01:37 -0500

    feat se crea base de dato

---
+++ base_datos.py
@@ -0,0 +1,47 @@
+#importar  la extencion sqlite3 
+import sqlite3
+#crae variables de conexion 
+conexion = sqlite3.connect("Base_de_datos.db")#agregar ina base d edatos como existente 
+curso_db = conexion.cursor()#crear n cursos de la base de datos 
+#crar tabla de base de dato si no existe 
+curso_db.execute(
+    """CREATE TABLE IF NOT EXISTS usuario(       
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    edad  INTEGER,
+    correo TEXT UNIQUE
+    )
+    """
+) 
+conexion.commit()
+#insetar  un solo registro  en la tabla 
+curso_db.execute("INSERT INTO usuario (nombre,edad,correo)VALUES(?,?,?)",
+                 ("luisa",29,"cal@email.com")
+                 )
+conexion.commit()
+#insertar  multiples registro 
+usuario_nuevos = [("juana",29,"lj@email.com"),("jefersonr",30,"jn@email.com")]
+#insertar varios usuario 
+curso_db.executemany("INSERT INTO usuario (nombre,edad,correo)VALUES(?,?,?)",
+                    usuario_nuevos)
+conexion.commit()
+#consultar  los datos en la tabla 
+curso_db.execute("SELECT * FROM usuario")
+usuario = curso_db.fetchall()#obtener todos los registro 
+#mostrar todos los resultados 
+for usuarios in usuario:
+    print(usuarios)
+#actializar datos 
+curso_db.execute("UPDATE usuario SET edad = ? WHERE nombre = ?",
+                 (35,"juan"))
+conexion.commit()
+#eliminar un dato especifico 
+curso_db.execute("DELETE FORM usuario   WHERE nombre=?",
+                 ("jenifer",))
+conexion.commit()
+#para eliminar todos los registros 
+curso_db.execute("DELETE FROM usuario")
+conexion.commit()
+
+#cerrar la conexion
+conexion.close()    
---
+++ practica.py
@@ -0,0 +1,23 @@
+import sqlite3
+#crar variables ce conexion 
+conn = sqlite3.connect("Base_de_datos_db")
+curso = conn.cursor()# el cursos de base de dato 
+#crear tabla 
+curso.execute(
+    """CREATE TABLE IF NOT EXISTS comentario(
+        id_comentario  INTEGRE PRIMARY KEY ,
+        tipo_de_comentariO TEXT NOT NULL,
+        fecha TEXT NOT NULL
+    )
+    """
+)
+conn.commit()
+curso.execute("INSERT INTO comentario (tipo_de_comentariO,fecha)VALUES(?,?)",
+              ("comentrio","2019,10,29"))
+conn.commit()
+curso.execute("SELECT * FROM comentario")
+comentario = curso.fetchall()
+for comentarios in comentario:
+        print(comentarios)
+#curso.execute("DELETE FROM  comentario")
+#conn.commit()        
